823977ad92cfb010a9c093871a00ab65bb2a31ea
deober <derick.ober@gmail.com>
2024-10-24 14:38:51 -0700

    Updated setup to include dependencies.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
+from setuptools import setup
+
 __version__ = "0.1"
 
-# Available at setup time due to pyproject.toml
-from setuptools import setup
 
 setup(
     name="phaselocker",
     version=__version__,
     packages=["phaselocker"],
-    install_requires=[],
+    install_requires=["numpy", "scipy"],
 )
