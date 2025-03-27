7cdc851433710169262c52b774d8fb628a3c5cb7
deober <derick.ober@gmail.com>
2025-03-26 17:23:49 -0700

    Updated setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,8 @@ __version__ = "0.1"
 
 setup(
     name="phaselocker",
+    author="Derick Ober",
     version=__version__,
     packages=["phaselocker"],
-    install_requires=["numpy", "scipy","bokeh"],
+    install_requires=["numpy", "scipy", "bokeh"],
 )
